4a7280c0836fdec60e4cf936cac58d5656116ce0
Andrea <16215183+andreamaestri@users.noreply.github.com>
2024-11-08 17:18:54 +0000

    Added FaveFood component

---
+++ src/FaveFood.jsx
@@ -0,0 +1,19 @@
+export default function FaveFood() {
+	return (
+		<>
+			<article>
+				<h1>My favourite food is...</h1>
+				<div>
+					<ul>
+						<li>Pizza</li>
+						<li>Lasgna</li>
+						<li>Roast!</li>
+						<li>Chips</li>
+						<li>Tom Yum</li>
+						<li>Savoy Cabbage stew</li>
+					</ul>
+				</div>
+			</article>
+		</>
+	);
+}
